53c559667aa88e493f67e9f7ea71b44462ad1d70
Nicholas Bingei <nbinged@gmail.com>
2019-09-21 22:14:29 +0800

    react build test

--- tables.sql
+++ tables.sql
@@ -1,43 +1,7 @@
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username TEXT,
-    password TEXT
+-- create pokemons table
+CREATE TABLE IF NOT EXISTS products (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	price TEXT,
+	description TEXT
 );
-
-CREATE TABLE IF NOT EXISTS journals (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    username TEXT,
-    journal_date DATE,
-    skin_rating INTEGER,
-    journal_notes TEXT,
-    image_file TEXT,
-    create_at TIMESTAMP DEFAULT now()
-);
-
-CREATE TABLE IF NOT EXISTS routines (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    cleanser TEXT,
-    toner TEXT,
-    moisturizer TEXT,
-    treatment TEXT,
-    sunblock TEXT,
-    create_at TIMESTAMP DEFAULT now()
-);
-
--- //FOR MANY TO MANY TABLES
--- CREATE TABLE IF NOT EXISTS users_clothing (
---     id SERIAL PRIMARY KEY,
---     user_id INTEGER,
---     clothing_id INTEGER,
---     FOREIGN KEY (user_id) REFERENCES users (id),
---     FOREIGN KEY (clothing_id) REFERENCES clothing (id)
--- );
-
--- CREATE TABLE IF NOT EXISTS outfits (
---     id SERIAL PRIMARY KEY,
---     user_id INTEGER,
---     item_id INTEGER
-    -- FOREIGN KEY () REFERENCES  (id),
-    -- FOREIGN KEY () REFERENCES  (id)
